c1f8904cd1423876c8dae33a08cb11fa205d4843
ArielMartin-Tech4Life <Tech4LifeandBeyond@gmail.com>
2026-02-12 12:26:56 -0500

    chore: fix ruff re-export warnings in security package

--- kivai_sdk/security/__init__.py
+++ kivai_sdk/security/__init__.py
@@ -1,1 +1,9 @@
-from .policy import evaluate_authorization, required_role_for_intent
+from .policy import (
+    evaluate_authorization as evaluate_authorization,
+    required_role_for_intent as required_role_for_intent,
+)
+
+__all__ = [
+    "evaluate_authorization",
+    "required_role_for_intent",
+]
